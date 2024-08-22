f0f05d89a6d94a6c5e7f450e659481b6f0bf3f7c
dave <horsepin@protonmail.com>
2024-08-22 19:57:13 +0100

    - moved `Attract.cs` from the game project to Domain.

---
+++ NetPinProc.Domain/Mode/Attract.cs
@@ -0,0 +1,35 @@
+﻿namespace NetPinProc.Domain.Mode
+{
+    /// <summary>Base attract mode that just handles starting a game</summary>
+    public class Attract : Mode
+    {
+        /// <summary>
+        /// Init
+        /// </summary>
+        /// <param name="game"></param>
+        /// <param name="priority"></param>
+        public Attract(IGameController game, int priority) : base(game, priority) { }
+
+        /// <summary>Start button, starts game and adds a player if the trough is full</summary>
+        /// <param name="sw"></param>
+        /// <returns></returns>
+        public virtual bool sw_start_active(Switch sw)
+        {
+            Game.Logger?.Log("start button active", PinProc.LogLevel.Debug);
+            if (Game.Trough?.IsFull() ?? false) //todo: credit check?
+            {
+                Game.Logger.Log("start button, trough full", PinProc.LogLevel.Debug);
+                Game.StartGame();
+                Game.AddPlayer();
+                Game.StartBall();
+                Game.Modes.Remove(this);
+            }
+            else
+            {
+                Game.Logger?.Log("attract start. trough balls:" + _game.Trough.NumBalls(), PinProc.LogLevel.Debug);
+                //TODO: Ball search
+            }
+            return SWITCH_CONTINUE;
+        }
+    }
+}
