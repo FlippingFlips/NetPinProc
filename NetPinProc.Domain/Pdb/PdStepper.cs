4692165ffcfb6c1748af1adaa4b8c78af1f307b9
dave <horsepin@protonmail.com>
2024-08-26 04:43:43 +0100

    - added `GetDuration` for a PdStepper and `MoveReturnDurationMs` to return a delay in milliseconds when moving

--- NetPinProc.Domain/Pdb/PdStepper.cs
+++ NetPinProc.Domain/Pdb/PdStepper.cs
@@ -1,13 +1,18 @@
-﻿namespace NetPinProc.Domain.Pdb
+﻿using System;
+
+namespace NetPinProc.Domain.Pdb
 {
-    /// <summary>PDLEd board stepper</summary>
+    /// <summary>PDLEd board stepper <para/>
+    /// General stepper motor notes:<para/>
+    ///    - Full step motors require 200 steps (1.8 degrees step angle), half step motors 400 steps (0.9 degrees)<para/>
+    ///    </summary>
     public class PdStepper
     {
         /// <inheritdoc/>
         public readonly uint BoardAddress;
 
+        const int TIME = 32000000;
         private readonly IPDLED board;
-
         /// <summary>Sets up a stepper and registering the speed</summary>
         /// <param name="proc"></param>
         /// <param name="name"></param>
@@ -15,7 +20,12 @@
         /// <param name="stepperIndex"></param>
         /// <param name="speed"></param>
         /// <param name="stopSw"></param>
-        public PdStepper(IProcDevice proc, string name, uint boardId, byte stepperIndex, uint speed, Switch stopSw = null)
+        public PdStepper(IProcDevice proc,
+            string name,
+            uint boardId,
+            byte stepperIndex,
+            uint speed,
+            Switch stopSw = null)
         {
             Name = name;
             BoardAddress = boardId;
@@ -48,7 +58,21 @@
         /// <summary></summary>
         public Switch StopSwitch { get; set; }
 
-        /// <inheritdoc/>
+        /// <summary>Gets a duration for the movement position and speed set on the stepper<para/>
+        /// Don't know how accurate or is the working the same as proc_devices.py.. MPF move_rel_pos</summary>
+        /// <param name="pos"></param>
+        /// <returns></returns>
+        public int GetDuration(int pos)
+        {
+            var totalMs = TimeSpan
+                    .FromMilliseconds(((Math.Abs(pos) * 2 * Speed) / TIME) + 0.3)
+                    .TotalMilliseconds;
+
+            return (int)(totalMs * 1000);
+        }
+
+        /// <summary></summary>
+        /// <param name="pos"></param>
         public virtual void Move(int pos)
         {
             var sIndex = 23 + StepperIndex;
@@ -60,10 +84,14 @@
             }
         }
 
-        /// <summary>
-        /// register the stepper speed = Stepper Ticks Per Half Period <para/>
-        /// Try speeds of 2500 for fast and higher values for slow, this is totally dependent on your driver and stepper though
-        /// </summary>
+        public virtual int MoveReturnDurationMs(int pos)
+        {
+            var duration = GetDuration(pos);
+            Move(pos);
+            return duration;
+        }
+
+        /// <summary>register the stepper speed = Stepper Ticks Per Half Period <para/></summary>
         /// <param name="speed"></param>
         public virtual void SetSpeed(uint speed)
         {
