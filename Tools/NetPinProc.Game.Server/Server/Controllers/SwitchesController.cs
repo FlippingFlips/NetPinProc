d7dceb37d392dae2efedabe97b02a1585ca9d185
dave <horsepin@protonmail.com>
2024-08-29 01:44:40 +0100

    - updated switches page and controller to edit, add and delete. updated mudblazor, fixes validation on forms

--- Tools/NetPinProc.Game.Server/Server/Controllers/SwitchesController.cs
+++ Tools/NetPinProc.Game.Server/Server/Controllers/SwitchesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NetPinProc.Domain.Exceptions;
 using NetPinProc.Domain.MachineConfig;
 using NetPinProc.Game.Sqlite;
 
@@ -17,6 +18,23 @@ namespace NetPinProc.Game.Manager.Server.Controllers
         public async Task<IEnumerable<SwitchConfigFileEntry>> OnGetAsync() => 
             await _netProcDb.Switches.ToListAsync();
 
+        [HttpPost]
+        public async Task<ActionResult<SwitchConfigFileEntry>> OnPostAsync([FromBody] SwitchConfigFileEntry swConfig)
+        {
+            try
+            {
+                _netProcDb.Switches.Add(swConfig);
+                await _netProcDb.SaveChangesAsync();
+
+                return Ok(swConfig);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                return BadRequest($"{ex.Message} - {ex.InnerException?.Message}");
+            }
+        }
+
         [HttpPut]
         public async Task<bool> OnPutAsync([FromBody] SwitchConfigFileEntry swConfig)
         {
@@ -31,6 +49,26 @@ namespace NetPinProc.Game.Manager.Server.Controllers
             {
                 _logger.LogError(ex.ToString());
                 return false;
+            }
+        }
+
+        [HttpDelete("{swNum}")]
+        public async Task<ActionResult<int>> OnDeleteAsync(string swNum)
+        {
+            try
+            {
+                var item = await _netProcDb.Switches.FirstOrDefaultAsync(x => x.Number == swNum);
+
+                if (item == null) throw new SwitchNotFoundException($"{swNum} doesn't exist to delete!");
+
+                _netProcDb.Switches.Remove(item);
+
+                return Ok(await _netProcDb.SaveChangesAsync());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                return BadRequest($"{ex.Message} - {ex.InnerException?.Message}");
             }
         }
 
