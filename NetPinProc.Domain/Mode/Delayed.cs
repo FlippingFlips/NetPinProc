bcfa8ee4002becf29e5c41cb038cac7fc17a2d34
dave <horsepin@protonmail.com>
2024-09-01 05:18:50 +0100

    - changed `IMode.Delay()` to use a `params object[]` and removed the single param option.

--- NetPinProc.Domain/Mode/Delayed.cs
+++ NetPinProc.Domain/Mode/Delayed.cs
@@ -5,28 +5,26 @@ namespace NetPinProc.Domain.Mode
 {
     internal class Delayed : IComparable<Delayed>
     {
-        public Delayed(string Name, double Time, Delegate Handler = null, EventType Event_Type = EventType.SwitchClosedDebounced, object Param = null)
+        public Delayed(string Name,
+            double Time,
+            Delegate Handler = null, 
+            EventType Event_Type = EventType.SwitchClosedDebounced,
+            params object[] Params)
         {
             this.Name = Name;
             this.Time = Time;
             this.Handler = Handler;
             this.Event_Type = Event_Type;
-            this.Param = Param;
+            this.Params = Params;
         }
 
         public EventType Event_Type { get; set; }
         public Delegate Handler { get; set; }
         public string Name { get; set; }
-        public object Param { get; set; }
+        public object[] Params { get; set; }
         public double Time { get; set; }
-        public int CompareTo(Delayed other)
-        {
-            return other.Time.CompareTo(Time);
-        }
+        public int CompareTo(Delayed other) => other.Time.CompareTo(Time);
 
-        public override string ToString()
-        {
-            return string.Format("name={0} time={1} event_type={2}", Name, Time, Event_Type);
-        }
+        public override string ToString() => string.Format("name={0} time={1} event_type={2}", Name, Time, Event_Type);
     }
 }
