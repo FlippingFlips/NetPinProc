960ff197d2843300af88e3f8c6868d71b5e55ef7
dave <horsepin@protonmail.com>
2024-09-04 22:39:31 +0100

    added some constants for `MachineItemColors` and `Names` of common collections

---
+++ NetPinProc.Domain/Constants/Names.cs
@@ -0,0 +1,13 @@
+﻿namespace NetPinProc.Domain.Constants
+{
+    public class Names
+    {
+        public const string SWITCHES = nameof(SWITCHES);
+        public const string LEDS = nameof(LEDS);
+        public const string LAMPS = nameof(LAMPS);
+        public const string DRIVERS = nameof(DRIVERS);
+        public const string STEPPERS = nameof(STEPPERS);
+        public const string SERVOS = nameof(SERVOS);
+
+    }
+}
