4186f19f1a4892e79c9ecce487cc7e16c7a758f1
dave <horsepin@protonmail.com>
2024-09-04 22:41:30 +0100

    - added quick api method to return list of machine items.

--- Tools/NetPinProc.Game.Server/Server/Controllers/MachineController.cs
+++ Tools/NetPinProc.Game.Server/Server/Controllers/MachineController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NetPinProc.Domain.Constants;
 using NetPinProc.Game.Sqlite;
 using NetPinProc.Game.Sqlite.Model;
 
@@ -19,5 +20,54 @@ namespace NetPinProc.Game.Manager.Server.Controllers
         /// <returns></returns>
         public async Task<Machine> OnGetAsync() =>
             await _netProcDb.Machine.FirstAsync();
+
+        /// <summary>Just a quick bog standard text print of all items in the machine</summary>
+        /// <returns></returns>
+        [HttpGet("MachineItemListText")]
+        public async Task<string> GetMachineItemListsAsync()
+        {
+            var switches = await _netProcDb.Switches.AsNoTracking()
+                .OrderBy(x => x.Number)
+                .Select(x=>x.Name).ToListAsync();
+            var lamps = await _netProcDb.Lamps.AsNoTracking().Select(x => x.Name).ToListAsync();
+            var leds = await _netProcDb.Leds.AsNoTracking().Select(x => x.Name).ToListAsync();
+            var drivers = await _netProcDb.Coils.AsNoTracking().Select(x => x.Name).ToListAsync();
+            var steppers = await _netProcDb.Steppers.AsNoTracking().Select(x => x.Name).ToListAsync();
+            var servos = await _netProcDb.Servos.AsNoTracking().Select(x => x.Name).ToListAsync();
+
+            var result = string.Empty;
+            if(switches != null)
+            {
+                result += $"\n{Names.SWITCHES}\n";
+                foreach(var mi in switches) { result += $"{mi}\n"; }
+            }
+            if (lamps != null)
+            {
+                result += $"\n{Names.LAMPS}\n";
+                foreach (var mi in lamps) { result += $"{mi}\n"; }
+            }
+            if (leds != null)
+            {
+                result += $"\n{Names.LEDS}\n";
+                foreach (var mi in leds) { result += $"{mi}\n"; }
+            }
+            if (drivers != null)
+            {
+                result += $"\n{Names.DRIVERS}\n";
+                foreach (var mi in drivers) { result += $"{mi}\n"; }
+            }
+            if (steppers != null)
+            {
+                result += $"\n{Names.STEPPERS}\n";
+                foreach (var mi in steppers) { result += $"{mi}\n"; }
+            }
+            if (servos != null)
+            {
+                result += $"\n{Names.SERVOS}\n";
+                foreach (var mi in servos) { result += $"{mi}\n"; }
+            }
+
+            return result;
+        }
     }
 }
