815473d5b05a5550589a75b8b33d62d7ab65fa4e
dave <horsepin@protonmail.com>
2024-09-04 23:25:26 +0100

    - added MachineList and SvgElement, fixes ScgToMachineItems

---
+++ NetPinProc.Domain/MachineConfig/MachineList.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace NetPinProc.Domain.MachineConfig
+{
+    /// <summary>List with collection name</summary>
+    /// <typeparam name="T"></typeparam>
+    public class MachineList<T> : List<T>
+    {
+        public string CollectionName { get; set; }
+    }
+}
---
+++ Tools/NetPinProc.Game.Server/Shared/Tools/Playfield/SvgElement.cs
@@ -0,0 +1,14 @@
+﻿namespace NetPinProc.Game.Manager.Shared.Tools.Playfield
+{
+    public class SvgElement
+    {
+        public string Name { get; set; }
+        /// <summary>type eg:rect</summary>
+        public string Type { get; set; }
+        public string Width { get; set; }
+        public string Height { get; set; }
+        public string X { get; set; }
+        public string Y { get; set; }
+        public string Path { get; set; }
+    }
+}
