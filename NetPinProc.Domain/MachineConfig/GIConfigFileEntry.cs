76c3bbd5d925e9023e35feecea55ecbcd12bce61
dave <horsepin@protonmail.com>
2024-09-07 18:46:30 +0100

    - added Polarity to GIConfigItem

--- NetPinProc.Domain/MachineConfig/GIConfigFileEntry.cs
+++ NetPinProc.Domain/MachineConfig/GIConfigFileEntry.cs
@@ -3,7 +3,9 @@
     /// <summary>Represents a GI string list element in memory</summary>
     public class GIConfigFileEntry : ConfigFileEntryBase
     {
-        public string Number { get; set; }        
+        public string Number { get; set; }
+        ///<inheritdoc/>
+        public bool Polarity { get; set; }
         public string Tags { get; set; }
     }
 }
