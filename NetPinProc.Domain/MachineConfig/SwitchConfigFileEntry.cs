f1f9c3aaf914f0d36dacc1da15723a32db5646a5
dave <horsepin@protonmail.com>
2024-09-13 14:07:13 +0100

    added: `IProcNumber` to some machine Config File entries added: `NonDebounce` to `SwitchConfigFileEntry` for debounce options on the switch added: `SingleColor` on `LedConfigFileEntry`. PDLed boards can also do 84 single color

--- NetPinProc.Domain/MachineConfig/SwitchConfigFileEntry.cs
+++ NetPinProc.Domain/MachineConfig/SwitchConfigFileEntry.cs
@@ -1,17 +1,21 @@
-﻿using NetPinProc.Domain.PinProc;
+﻿using NetPinProc.Domain.Interface;
+using NetPinProc.Domain.PinProc;
 using System.ComponentModel.DataAnnotations;
 
 namespace NetPinProc.Domain.MachineConfig
 {
     /// <summary>Represents the switch config entry in memory<para/>
     /// TODO: needs a de-bounce option?</summary>
-    public class SwitchConfigFileEntry : ConfigFileEntryBase
+    public class SwitchConfigFileEntry : ConfigFileEntryBase, IProcNumber
     {
         /// <inheritdoc/>
         public SwitchType Type { get; set; }
 
         /// <inheritdoc/>
         public string Number { get; set; }
+
+        /// <summary>de-bounced switch rules are set by default, set to non debounce</summary>
+        public bool? NonDebounce { get; set; }
 
         /// <inheritdoc/>
         public string Tags { get; set; }
