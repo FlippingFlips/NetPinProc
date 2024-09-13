2ef3de651be8bb91336cbd52f230397c139ed499
dave <horsepin@protonmail.com>
2024-09-13 14:10:22 +0100

    - added migrations Sqlite table changes

---
+++ NetPinProc.Game.Sqlite/Migrations/20240913000614_AddedSwitchDebouncAndSingleColorOptionToLed.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetPinProc.Game.Sqlite.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSwitchDebouncAndSingleColorOptionToLed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "NonDebounce",
+                table: "Switches",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "SingleColor",
+                table: "Leds",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NonDebounce",
+                table: "Switches");
+
+            migrationBuilder.DropColumn(
+                name: "SingleColor",
+                table: "Leds");
+        }
+    }
+}
